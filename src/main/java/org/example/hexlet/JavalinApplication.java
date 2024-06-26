21e78b5407c7986f7f17792ddc4bdcd2dbe89baa
Alex Belovitsky <MIRREXONE@gmail.com>
2024-06-26 07:42:48 +0300

    Rename main class

--- src/main/java/org/example/hexlet/JavalinApplication.java
+++ src/main/java/org/example/hexlet/JavalinApplication.java
@@ -2,7 +2,7 @@ package org.example.hexlet;
 
 import io.javalin.Javalin;
 
-public class HelloWorld {
+public class JavalinApplication {
     public static void main(String[] args) {
         var app = Javalin.create(config -> {
            config.bundledPlugins.enableDevLogging();
